e0ea929ec68978d9df34e1ea829005710b0d9d21
Laura Del Duca <laura.delduca1990@gmail.com>
2019-03-29 10:16:56 +0100

    add staggering effect

--- js/main.js
+++ js/main.js
@@ -12,16 +12,16 @@ new ScrollMagic.Scene({
     // Dan ce cas-ci, on veut qu'elle commence quand l'élément entre sur la page: “onEnter”
     triggerHook: "onEnter",
 })
-// On détermine la durée de l'animation
-.duration('400%')
-// Avec .setTween, on ajoute l'animation GSAP à la scène
-.setTween("#parallax", {
-    backgroundPosition: "0% 100%",
-    ease: Linear.easeNone
-})
-.addIndicators() 
-// Quand notre scène est configurée, on l'ajoute au controlleur
-.addTo(controller);
+    // On détermine la durée de l'animation
+    .duration('400%')
+    // Avec .setTween, on ajoute l'animation GSAP à la scène
+    .setTween("#parallax", {
+        backgroundPosition: "0% 100%",
+        ease: Linear.easeNone
+    })
+    // .addIndicators()
+    // Quand notre scène est configurée, on l'ajoute au controlleur
+    .addTo(controller);
 
 
 // L'animation suivante est le "slide and pin" pour les slides 1 et 2
@@ -34,18 +34,18 @@ new ScrollMagic.Scene({
     triggerHook: "onLeave",
 })
 
-.setPin("#slidein")
-.addIndicators()
-.addTo(controller);
+    .setPin("#slidein")
+    // .addIndicators()
+    .addTo(controller);
 
 // ------------------------------------------------- SLIDE 2
 new ScrollMagic.Scene({
     triggerElement: "#slidein2",
     triggerHook: "onLeave",
 })
-.setPin("#slidein2")
-.addIndicators()
-.addTo(controller);
+    .setPin("#slidein2")
+    // .addIndicators()
+    .addTo(controller);
 
 // Sur la dernière slide, il y a trois animations et elles requièrent les timelines de GSAP 
 
@@ -54,8 +54,8 @@ new ScrollMagic.Scene({
 var leftword = new TimelineMax();
 // L'objet dans être en dehors de l'écran quand on arrive sur la slide
 // On le moet donc en position initiale à -500
-var fromLeftFrom = TweenMax.from("#left", 1, {x: -500});
-var fromLeftTo = TweenMax.to("#left", 3, {x: 0});
+var fromLeftFrom = TweenMax.from("#left", 1, { x: -500 });
+var fromLeftTo = TweenMax.to("#left", 3, { x: 0 });
 leftword
     .add(fromLeftFrom)
     .add(fromLeftTo);
@@ -67,48 +67,48 @@ new ScrollMagic.Scene({
     // Le point de départ est de 200 px au dessous du haut de la slide 2
     offset: 200,
 })
-// On ajoute notre timeline via .setTween()
-.setTween(leftword)
-// Et on détermine la durée de l'animation à un scroll de 400px
-.duration(400)
-.reverse(true)
-.addIndicators()
-.addTo(controller);
+    // On ajoute notre timeline via .setTween()
+    .setTween(leftword)
+    // Et on détermine la durée de l'animation à un scroll de 400px
+    .duration(400)
+    .reverse(true)
+    // .addIndicators()
+    .addTo(controller);
 
 // ------------------------------------------------- CENTRAL WORD - OPACITY
 var opacity = new TimelineMax();
-var fadeInFrom = TweenMax.from("#opacity", 1, {opacity: 0});
-var fadeInTo = TweenMax.to("#opacity", 1, {opacity: 1});
+var fadeInFrom = TweenMax.from("#opacity", 1, { opacity: 0 });
+var fadeInTo = TweenMax.to("#opacity", 1, { opacity: 1 });
 opacity
     .add(fadeInFrom)
     .add(fadeInTo);
- 
+
 new ScrollMagic.Scene({
-        triggerElement: "#slidein2",
-        offset: 200,
-    })
+    triggerElement: "#slidein2",
+    offset: 200,
+})
     .setTween(opacity)
     .duration(400)
     .reverse(true)
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
- 
+
 // ------------------------------------------------- WORD FROM THE BOTTOM
 var rightword = new TimelineMax();
-var fromBottomFrom = TweenMax.from("#bottom", 1, {y: 300});
-var fromBottomTo = TweenMax.to("#bottom", 3, {y: 0});
+var fromBottomFrom = TweenMax.from("#bottom", 1, { y: 300 });
+var fromBottomTo = TweenMax.to("#bottom", 3, { y: 0 });
 rightword
     .add(fromBottomFrom)
     .add(fromBottomTo);
- 
+
 new ScrollMagic.Scene({
-        triggerElement: "#slidein2",
-        offset: 200,
-    })
+    triggerElement: "#slidein2",
+    offset: 200,
+})
     .setTween(rightword)
     .duration(400)
     .reverse(true)
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 
 // ------------------------------------------------- SLIDE 3 - CARDS
@@ -116,48 +116,76 @@ new ScrollMagic.Scene({
     triggerElement: "#slidein3",
     triggerHook: "onLeave",
 })
-.setPin("#slidein3")
-.addIndicators()
-.addTo(controller);
+    .setPin("#slidein3")
+    // .addIndicators()
+    .addTo(controller);
 
-// ------------------------------------------------- LEFT BOX
+// ------------------------------------------------- SLIDE 3 - TEXT BOXES
 var leftbox = new TimelineMax();
-var leftboxFrom = TweenMax.staggerFrom(".box", 1, {x:-300, opacity:0}, 0.5);
+var leftboxFrom = TweenMax.staggerFrom(".box", 1, { x: -300, opacity: 0 }, 0.5);
 // var leftboxTo = TweenMax.staggerTo(".box", 1, {x: 0});
 leftbox
     .add(leftboxFrom)
-    // .add(leftboxTo)
+// .add(leftboxTo)
 
 new ScrollMagic.Scene({
     triggerElement: "#slidein3",
     offset: 250,
 })
-.setTween(leftbox)
-.reverse(true)
-.addIndicators()
-.addTo(controller);
+    .setTween(leftbox)
+    .reverse(true)
+    // .addIndicators()
+    .addTo(controller);
 
-// ------------------------------------------------- SLIDE 4 - PHOTO and TEXTE
-// Photo en zoom (.to scale???)
-// Texte: plusieurs lignes en stagger
+// ------------------------------------------------- SLIDE 4
 new ScrollMagic.Scene({
     triggerElement: "#slidein4",
     triggerHook: "onLeave",
 })
-.setPin("#slidein4")
-.addIndicators()
-.addTo(controller);
+    .setPin("#slidein4")
+    // .addIndicators()
+    .addTo(controller);
 
 // ------------------------------------------------- SLIDE 4 - PHOTO
 var zoommountain = new TimelineMax();
-var mountainzoom = TweenMax.to("#mountain2", 1, {scale:2.5});
+var mountainzoom = TweenMax.to("#mountain2", 1, { scale: 2.5 });
 zoommountain
     .add(mountainzoom)
 
 new ScrollMagic.Scene({
     triggerElement: "#slidein4",
 })
-.setTween(zoommountain)
-.reverse(true)
-.addIndicators()
-.addTo(controller);
+    .setTween(zoommountain)
+    .reverse(true)
+    // .addIndicators()
+    .addTo(controller);
+
+// ------------------------------------------------- SLIDE 4 - TEXT LINES
+var stagger = new TimelineMax();
+var staggerlines = TweenMax.staggerFrom(".staggerline", 1, {
+    opacity: 0,
+    x: 500,
+    delay: 0.5
+}, 1);
+stagger
+    .add(staggerlines)
+
+new ScrollMagic.Scene({
+    triggerElement: "#slidein4",
+})
+    .setTween(stagger)
+    .reverse(true)
+    .addIndicators()
+    .addTo(controller);
+
+
+// TweenMax.staggerFrom(
+//     ".box",
+//     0.5,
+//     {
+//       opacity: 0,
+//       y: 200,
+//       delay: 0.5
+//     },
+//     0.2
+//   );
